e988e94828f25367504c582261958beccec4acea
KhineZinThaw7 <scm.khinezinthaw@gmail.com>
2022-06-19 18:20:22 +0630

    make middleware

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,16 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/posts', [PostController::class, 'index']);
-// Route::get('/posts/create', [PostController::class, 'create']);
-// Route::post('/posts', [PostController::class, 'store']);
-// Route::get('/posts/{id}/edit', [PostController::class, 'edit']);
-// Route::put('/posts/{id}', [PostController::class, 'update']);
-// Route::get('/posts/{id}', [PostController::class, 'show']);
-// Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create'])->middleware('myAuth');
+Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/{id}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{id}', [PostController::class, 'update']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::delete('/posts/{id}', [PostController::class, 'destroy']);
 
 //post, category
-Route::resource('posts', PostController::class);
+// Route::resource('posts', PostController::class);
 Route::resource('categories', CategoryController::class);
 
 // register
